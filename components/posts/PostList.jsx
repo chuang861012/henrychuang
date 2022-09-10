92037e095b722ac1f769f400dffc71d68d0c75a2
Chuang <33791604+chuang861012@users.noreply.github.com>
2022-09-10 17:46:33 +0800

    sort posts by date

--- components/posts/PostList.jsx
+++ components/posts/PostList.jsx
@@ -12,20 +12,22 @@ const getDateString = (date, locale) => {
 function PostList({ posts, locale, selectedLanguage }) {
     return (
         <ul className={classes.posts}>
-            {posts.map((post) => {
-                if (selectedLanguage !== "all" && selectedLanguage !== post.language) return null;
-                return (
-                    <li key={post.slug}>
-                        <PostItem
-                            title={post.title}
-                            slug={post.slug}
-                            date={getDateString(post.date, locale)}
-                            excerpt={post.excerpt}
-                            language={post.language}
-                        />
-                    </li>
-                );
-            })}
+            {posts
+                .sort((a, b) => new Date(b.date) - new Date(a.date))
+                .map((post) => {
+                    if (selectedLanguage !== "all" && selectedLanguage !== post.language) return null;
+                    return (
+                        <li key={post.slug}>
+                            <PostItem
+                                title={post.title}
+                                slug={post.slug}
+                                date={getDateString(post.date, locale)}
+                                excerpt={post.excerpt}
+                                language={post.language}
+                            />
+                        </li>
+                    );
+                })}
         </ul>
     );
 }
